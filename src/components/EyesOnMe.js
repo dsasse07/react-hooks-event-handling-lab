6b12b6139af9bc6d44dfc066c8e4c8838203d483
Daniel Sasse <dsasse07@gmail.com>
2021-02-22 15:46:31 -0500

    commit tree 752e7eb1dc51f7c60aebdf31b863b51dca35e775

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function inFocus() {
+  console.log("Good!")
+}
+
+function lostFocus() {
+  console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe(props) {
+  return (
+    <button onFocus={inFocus} onBlur={lostFocus} >Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
